5a285a4204a268715451f4ef8e32e5e977ec2db3
Chris Pickford <chris@luminousuk.com>
2018-11-16 15:51:15 +0000

    Photobombth module

---
+++ electron-photobombth/src/images.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const fs = require("fs");
+const shell = require("electron").shell;
+const spawn = require("child_process").spawn;
+
+const logError = err => err && console.error(err);
+
+let images = [];
+
+exports.save = (picturesPath, contents, done) => {
+    const base64data = contents.replace(/^data:image\/png;base64,/, "");
+    const imgPath = path.join(picturesPath, `${new Date().toISOString().slice(0,16).replace(/:/g, "")}.png`);
+    fs.writeFile(imgPath, base64data, { encoding: "base64" }, err => {
+        if (err) {
+            return logError(err);
+        }
+        done(null, imgPath);
+    })
+};
+
+exports.getPicturesDir = app => {
+    return path.join(app.getPath("pictures"), "photobombth");
+};
+
+exports.mkdir = picturesPath => {
+    fs.stat(picturesPath, (err, stats) => {
+        if (err && err.code !== "ENOENT") {
+            return logError(err);
+        }
+        else if (err || !stats.isDirectory()) {
+            fs.mkdir(picturesPath, logError);
+        }
+    })
+};
+
+exports.getFromCache = index => {
+    return images[index];
+};
+
+exports.cache = imgPath => {
+    images = images.concat([imgPath]);
+    return images;
+};
+
+exports.remove = (index, done) => {
+    fs.unlink(images[index], err => {
+        if (err) {
+            return logError(err);
+        }
+        images.splice(index, 1);
+        done();
+    });
+};
+
+const openCmds = {
+    darwin: "open",
+    win32: "explorer",
+    linux: "nautilus"
+};
+
+exports.openDir = dirPath => {
+    const cmd = openCmds[process.platform];
+    if (cmd) {
+        spawn(cmd, [dirPath]);
+    } else {
+        shell.showItemInFolder(dirPath);
+    }
+}
